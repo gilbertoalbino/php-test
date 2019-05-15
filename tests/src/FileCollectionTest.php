d60f39b92ca313f3fd8735715713a33b9b1b9595
Gilberto Albino <contato@gilbertoalbino.com.br>
2019-05-15 15:07:41 -0300

    Adiciona FileCollectionTest

---
+++ tests/src/FileCollectionTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Live\Collection;
+
+use PHPUnit\Framework\TestCase;
+
+class FileCollectionTest extends TestCase
+{
+
+    /**
+     * @testa
+     * @doesNotPerformAssertions
+     */
+    public function objectCanBeConstructed()
+    {
+        $collection = new FileCollection('collection.txt');
+        return $collection;
+    }
+
+    /**
+     * @test
+     */
+    public function collectionShouldSerializeStructure()
+    {
+        $filename =  uniqid(time()) . '.txt';
+
+        $collection = new FileCollection($filename);
+        $collection->set('index1', 'value1');
+
+        $serialized_struct = $collection->getSerializedStructure();
+
+        $unserialized_struct = unserialize($serialized_struct);
+
+        $this->assertEquals($unserialized_struct['data']['index1'], $collection->get('index1'));
+
+        @unlink($filename);
+    }
+
+    /**
+     * @test
+     */
+    public function collectionShouldUseDefaultStructIfFileDoesNotExist()
+    {
+        $filename = uniqid(time()) . '.txt';
+
+        $collection = new FileCollection($filename);
+
+        $struct = ['data' => null, 'metadata' => null];
+
+        $this->assertEquals($struct, $collection->getPreparedStructure());
+
+        @unlink($filename);
+    }
+
+    /**
+     * @test
+     */
+    public function collectionShouldUseFileStructIfFileExists()
+    {
+        $filename = dirname(__FILE__) . '/../data/collection.txt';
+
+        $collection = new FileCollection($filename);
+
+        $struct = [
+            'data' => ['index1' => 'value1'],
+            'metadata' => ['index1' => ['ttl' => 1558803596]],
+        ];
+
+        $collection->get('index1');
+
+        $this->assertEquals($struct, $collection->getPreparedStructure());
+    }
+
+    /**
+     * @test
+     */
+    public function collectionCanBeSaved()
+    {
+        $filename = dirname(__FILE__) . '/../data/' . uniqid(time()) . '.txt';
+
+        $collection = new FileCollection($filename);
+
+        $collection->set('index2', 'value2', 120);
+
+        $collection->save();
+
+        /**
+         * to check on a new instance.
+         */
+        $saved_collection = new FileCollection($filename, true);
+        $this->assertEquals('value2', $saved_collection->get('index2'));
+
+        @unlink($filename);
+    }
+}
